4278a6726ff38b989ad31b70789c4409bf8881bf
Martin Eastwood <martin@Martins-MacBook-Pro.local>
2022-07-31 12:29:56 +0100

    added pytest

--- test/test_rps.py
+++ test/test_rps.py
@@ -1,16 +1,8 @@
-import unittest
 import penaltyblog as pb
 
 
-class TestRps(unittest.TestCase):
-    def test_rps(self):
-        predictions = [0.8, 0.1, 0.1]
-        observed = 0
-
-        rps_score = pb.utilities.rps(predictions, observed)
-
-        self.assertAlmostEqual(rps_score, 0.025, 4, "Should be 0.025")
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_rps():
+    predictions = [0.8, 0.1, 0.1]
+    observed = 0
+    rps_score = pb.utilities.rps(predictions, observed)
+    assert 0.024 < rps_score < 0.025
