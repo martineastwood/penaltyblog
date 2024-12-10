0fa5f9e06db76747b887dd288985a68ced2b504b
Martin Eastwood <martin@MacBook-Pro.local>
2024-12-10 22:03:09 +0000

    removed sofifa and espn scrapers as obselete

--- penaltyblog/scrapers/__init__.py
+++ penaltyblog/scrapers/__init__.py
@@ -1,5 +1,4 @@
 from .clubelo import ClubElo  # noqa
-from .espn import ESPN  # noqa
 from .fbref import FBRef  # noqa
 from .footballdata import FootballData  # noqa
 from .team_mappings import get_example_team_name_mappings  # noqa
