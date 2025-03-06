21da30fac4b6316af275d445b70d89349399556f
Martin Eastwood <martin@MacBook-Pro.local>
2025-03-06 20:56:47 +0000

    added another unit test to rps

--- test/test_metrics_rps.py
+++ test/test_metrics_rps.py
@@ -17,15 +17,9 @@ def test_rps():
     observed = [0, 1, 2]
     rps_score = pb.metrics.rps_array(predictions, observed)
     assert len(rps_score) == 3
-
     assert all(rps_score > 0)
 
-
-# def test_rps():
-#     predictions = [0.8, 0.1, 0.1]
-
-#     with pytest.raises(TypeError):
-#         _ = pb.metrics.rps(predictions, [0, 1])
-
-#     with pytest.raises(ValueError):
-#         _ = pb.metrics.rps(predictions, 5)
+    predictions = [0.8, 0.1, 0.1]
+    observed = 0
+    rps_score = pb.metrics.rps_array(predictions, [observed])
+    assert 0.024 < rps_score[0] < 0.025
