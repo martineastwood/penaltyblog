fb8dbbb5619697c5273257cd721e8d6cd9c9fc1b
Your Name <you@example.com>
2025-11-08 15:43:42 +0000

    tests: added more opta integration tests

--- test/test_opta_integration.py
+++ test/test_opta_integration.py
@@ -171,6 +171,22 @@ def test_matches_contestant():
 
 
 @pytest.mark.vcr
+def test_matches_contestant_home():
+    flow = opta.matches(
+        tournament_calendar_uuid=VALID_TMCL_UUID,
+        contestant_uuid=VALID_CONTESTANT_UUID,
+        contestant_position="home",
+    )
+    data = flow.collect()
+
+    assert data is not None
+    assert isinstance(data, list)
+    assert len(data) == 38
+    assert "matchInfo" in data[0]
+    assert "liveData" in data[0]
+
+
+@pytest.mark.vcr
 def test_fetch_match_events():
     # Use a real match UUID you have access to this week
     MATCH_UUID = "zhs8gg1hvcuqvhkk2itb54pg"
