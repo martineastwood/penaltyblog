925da1f747a4037d910031e031f41b56db821e80
Your Name <you@example.com>
2025-11-11 20:17:07 +0000

    tests: added more opta integration tests

--- test/test_opta_integration.py
+++ test/test_opta_integration.py
@@ -1305,3 +1305,20 @@ def test_pass_matrix():
     assert "liveData" in data[0]
     assert "id" in data[0]["matchInfo"]
     assert data[0]["matchInfo"]["id"] == VALID_FIXTURE_UUID
+
+
+@pytest.mark.vcr
+def test_pass_matrix_opta_names():
+    """
+    Tests: rankings() with tournament_calendar_uuid
+    """
+    flow = opta.pass_matrix(fixture_uuid=VALID_FIXTURE_UUID, use_opta_names=True)
+    data = flow.collect()
+
+    assert data is not None
+    assert isinstance(data, list)
+    assert len(data) > 0
+    assert "matchInfo" in data[0]
+    assert "liveData" in data[0]
+    assert "id" in data[0]["matchInfo"]
+    assert data[0]["matchInfo"]["id"] == VALID_FIXTURE_UUID
